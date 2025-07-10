6ada33f764c3db4ee0fac1da7e7cff346742986a
eng-Aly <es-aly.ahmed2027@alexu.edu.eg>
2025-07-10 20:52:30 +0300

    init keypad and interrupt drivers

--- HAL/LCD/LCD_Private.h
+++ HAL/LCD/LCD_Private.h
@@ -1,6 +1,8 @@
 #ifndef LCD_PRIVATE_H
 #define LCD_PRIVATE_H
 
-#define FUNCTION_SET_8BIT_2LS_5X10 0b00111100
+#define FUNCTION_SET_8BIT_2LS_5X10                       0b00111100
+#define ON_OFF_CONTROL_DISPLAY_CURSORS_BLINK             0b00001111
+#define DISPLAY_CLEAR                                    0b00000001
 
 #endif
--- HAL/LCD/LCD_Program.c
+++ HAL/LCD/LCD_Program.c
@@ -1,15 +1,6 @@
 #include "LCD_Header.h"
 
-void LCD_init_8bit(){
-    DIO_PINMode(CONTROL_DDR,E,OUTPUT);
-    DIO_PINMode(CONTROL_DDR,RS,OUTPUT);
-    DIO_PINMode(CONTROL_DDR,RW,OUTPUT);
 
-    DIO_PORTMode(DATA_DDR,OUTPUT);
-
-
-
-}
 
 void LCD_WriteCommand(u8 command){
     DIO_DigitalWrite(CONTROL_PORT,RS,LOW);
@@ -22,3 +13,21 @@ void LCD_WriteCommand(u8 command){
     _delay_ms(2);
     DIO_DigitalWrite(CONTROL_PORT,E,LOW);
 }
+void LCD_init_8bit(){
+    DIO_PINMode(CONTROL_DDR,E,OUTPUT);
+    DIO_PINMode(CONTROL_DDR,RS,OUTPUT);
+    DIO_PINMode(CONTROL_DDR,RW,OUTPUT);
+
+    DIO_PORTMode(DATA_DDR,OUTPUT);
+
+    LCD_WriteCommand(FUNCTION_SET_8BIT_2LS_5X10);
+    _delay_ms(3);
+    LCD_WriteCommand(ON_OFF_CONTROL_DISPLAY_CURSORS_BLINK);
+    _delay_ms(3);
+    LCD_WriteCommand(DISPLAY_CLEAR);
+
+
+
+}
+
+
